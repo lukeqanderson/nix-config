74e594777912e0ba52e5b0aadaf7fe41c5847113
lukeqanderson <lukeqanderson@gmail.com>
2025-02-09 20:52:40 -0800

    remove nvf module

--- flake.nix
+++ flake.nix
@@ -3,27 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nvf.url = "github:notashelf/nvf";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, nvf, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs: {
 
-    packages."x86_64-linux".default = (
-      nvf.lib.neovimConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-	modules = [ ./nvf-configuration.nix ];
-      }
-    ).neovim;
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./hosts/laptop/configuration.nix
         inputs.home-manager.nixosModules.default
-	nvf.nixosModules.default
       ];
     };
   };
